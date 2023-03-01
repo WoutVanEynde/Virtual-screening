550228f85347584bb9510406566127837d0cd713
Wout Van Eynde <99839278+WoutVanEynde@users.noreply.github.com>
2023-03-01 15:46:21 +0100

    Add files via upload

---
+++ ph4screening_commands_for_specs_run.sh
@@ -0,0 +1,2 @@
+/opt/moe2022/bin/moebatch -mpu 5 -script ph4screening_commands_for_specs.svl > ph4out_specs_firstround.log
+#screen -L ph4out_specs_firstround.log /opt/moe_2019.0102/bin-lnx64/moebatch -mpu 1 -script ph4screening_commands_for_specs.svl
